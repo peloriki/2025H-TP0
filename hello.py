df9e43418349a831e3d30a4f751dfd639d8ca627
Ricardo Acuna <ricardo-jose.acuna-de-aguas@polymtl.ca>
2025-01-16 13:50:32 -0500

    test 1 2 1 2

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+print ( math.pi * rayon ** 2)
+
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print ('ricardo acuna')
+
